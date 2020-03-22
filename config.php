e3149db195bd249ea7d487f39321386554f3f176
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2020-03-22 18:57:31 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'root');
+   define('DB_DATABASE', 'php2020');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   $host = "localhost"; /* Host name */
+   $user = "root"; /* User */
+   $password = "root"; /* Password */
+   $dbname = "php2020"; /* Database name */
+
+   $con = mysqli_connect($host, $user, $password,$dbname);
+
+   // Check connection
+   if (!$con) {
+   die("Connection failed: " . mysqli_connect_error());
+   }
+   ?>
