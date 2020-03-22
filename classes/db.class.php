60371572dc5af07e5df26492547a4c13367e48ee
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2020-03-22 18:57:12 +0100

    Update db.class.php

---
+++ classes/db.class.php
@@ -0,0 +1,19 @@
+<!-- hier komt de connectie naar de databank -->
+<?php
+
+ abstract class Db{
+   private static $conn;
+
+   public static function getInstance(){
+     $config = parse_ini_file("config.ini");
+     if(self::$conn != null){
+       return self::$conn;
+     }
+     else{
+       self::$conn = new PDO("mysql:host=localhost;dbname=" . $config['db_name'], $config['db_user'],$config['db_password']);
+       return self::$conn;
+     }
+     
+   }
+ }
+?>
