2e54684f2fbca86c6ee1a6fb00b246d4f0e03717
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2020-03-25 16:23:01 +0100

    Update db.class.php

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,19 +1,32 @@
 <!-- hier komt de connectie naar de databank -->
 <?php
+    abstract class Db {
+        private static $conn;
 
- abstract class Db{
-   private static $conn;
+        private static function getConfig(){
+            // get the config file
+            return parse_ini_file("config.ini");
+        }
+        
 
-   public static function getInstance(){
-     $config = parse_ini_file("config.ini");
-     if(self::$conn != null){
-       return self::$conn;
-     }
-     else{
-       self::$conn = new PDO("mysql:host=localhost;dbname=" . $config['db_name'], $config['db_user'],$config['db_password']);
-       return self::$conn;
-     }
-     
-   }
- }
-?>
+        public static function getInstance() {
+            if(self::$conn != null) {
+                // REUSE our connection
+                // echo "🚀";
+                return self::$conn;
+            }
+            else {
+                // CREATE a new connection
+
+                // get the configuration for our connection from one central settings file
+                $config = self::getConfig();
+                $database = $config['database'];
+                $user = $config['user'];
+                $password = $config['password'];
+
+                // echo "💥";
+                self::$conn = new PDO("mysql:host=localhost;dbname=php2020;", "root", "root", null);
+                return self::$conn;
+            }
+        }
+    }
