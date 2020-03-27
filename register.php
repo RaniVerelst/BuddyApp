2e99ea69210061f6f91f89ca3c480a50a24170f2
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-03-27 22:49:49 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,12 +1,14 @@
 <?php
-ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(-1);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require_once("classes/User.class.php");
 require_once("classes/Db.class.php");
 
 // valideren of alle velden zijn ingevuld
-if(!empty($_POST)){
+if (!empty($_POST)) {
   //return true;
   // sessie opstarten
   session_start();
@@ -30,25 +32,23 @@ if(!empty($_POST)){
   $user->getPassword();
   $user->setPassword_confirm($password_confirm);
   $user->getPassword_confirm();
-  try{
+  try {
     $result = $user->register();
-  }
-  catch(Exception $t){
+  } catch (Exception $t) {
     $error =  $t->getMessage();
   }
-
+} else {
+  // foutboodschap tonen
+  $empty_field_error = "Please, fill in all the fields";
 }
-  else{
-    // foutboodschap tonen
-    $empty_field_error = "Please, fill in all the fields";
-  }
 
-    // alles in orde? dan zullen we werken met getters en setters binnen User.class.php
+// alles in orde? dan zullen we werken met getters en setters binnen User.class.php
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -60,50 +60,50 @@ if(!empty($_POST)){
 </head>
 
 <body>
-  <form class="form_signup" method="post" action="">
+  <form name="register_form" class="form_signup" method="post" action="">
     <h1>Create your account</h1>
     <!-- foutboodschap wanneer niet alle velden zijn ingevuld -->
-    <?php if(isset($error)): ?>
-    <div class="error_signup"><?php echo $error; ?></div>
+    <?php if (isset($error)) : ?>
+      <div class="error_signup"><?php echo $error; ?></div>
     <?php endif; ?>
     <!-- firstname -->
     <div class="input_signup">
-      <input type="text"name="firstname"value="" placeholder="First name">
+      <input type="text" name="firstname" value="" placeholder="First name">
     </div>
 
     <!-- lastname -->
     <div class="input_signup">
-      <input type="text"name="lastname"value="" placeholder="Last name">
+      <input type="text" name="lastname" value="" placeholder="Last name">
     </div>
 
     <!-- username -->
     <div class="input_signup">
-    <input type="text"name="username"value="" placeholder="User name">
-  </div>
+      <input type="text" name="username" value="" placeholder="User name">
+    </div>
 
-  <!-- e-mail -->
-  <div class="input_signup">
-    <input type="text"name="email" placeholder="Email @student.thomasmore.be">
-  </div>
+    <!-- e-mail -->
+    <div class="input_signup">
+      <input type="text" name="email" placeholder="Email @student.thomasmore.be">
+    </div>
 
     <!-- password -->
     <div class="input_signup">
-      <input type="password"name="password" value="" placeholder="Password">
+      <input type="password" name="password" value="" placeholder="Password">
       <p>Your password need at least 8 characters</p>
     </div>
 
     <!-- confirm password -->
     <div class="input_signup">
-      <input type="password"name="password_confirm"value="" placeholder="Confirm Password">
+      <input type="password" name="password_confirm" value="" placeholder="Confirm Password">
     </div>
 
 
-      <!-- submit button -->
-      <input class="submit_signup" type="submit" value="Sign up">
+    <!-- submit button -->
+    <input class="submit_signup" type="submit" value="Sign up">
 
-      <div class="link_to_login">
-        <p>Do you already have an account?<a href="login.php">Sign in here</a></p>
-      </div>
+    <div class="link_to_login">
+      <p>Do you already have an account?<a href="login.php">Sign in here</a></p>
+    </div>
   </form>
 
 </body>
