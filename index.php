a84b49ad4a0d8d17d49592ffe6130ac1461573e5
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2020-04-03 16:08:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include_once("classes/Db.class.php");
 
   <!-- Theme CSS -->
   <link href="startbootstrap/css/freelancer.min.css" type="text/css" rel="stylesheet">
-  <link href="../css/style.css" type="text/css" rel="stylesheet">
+  <link href="css/style.css" type="text/css" rel="stylesheet">
 
 </head>
 
