a1c876ee6db9d43598177614c2592c5a539fcdca
Jens De Landtsheer <49272464+Jenz99@users.noreply.github.com>
2020-04-04 20:26:44 +0200

    Update search.php

--- search.php
+++ search.php
@@ -5,6 +5,8 @@ ini_set('display_errors', '1');
 require_once("classes/Db.class.php");
 include_once("classes/User.class.php");
 
+// _____________Naam_______________ //
+
 if(isset($_GET['search'])){
   $searchkey = $_GET['search'];
   $search_users = new User();
@@ -20,6 +22,25 @@ if(isset($_GET['search'])){
   $users = $u->showUser($id);
 }
 
+
+// _____________KENMERKEN _______________ //
+
+if(isset($_GET['search'])){
+  $searchkey = $_GET['search'];
+  $search_kenmerken = new User();
+  $result_kenmerken = $search_kenmerken->searchKenmerk($searchkey);
+
+  $conn = Db::getInstance();
+  $statement = $conn->prepare("select * from profile_details");
+  $statement->execute();
+  $kenmerken = $statement->fetchAll();
+    
+  $k = new User();
+  $id = "";
+  $kenmerken = $k->showKenmerk($id);
+}
+
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,8 +54,8 @@ if(isset($_GET['search'])){
 <body>
 
     <div class="search_results">
-      <?php if(count($result_users) > 0 ): ?>
-        <h1> <?php echo count($result_users) . " Search Result(s) found for " . "<span style = 'font-weight: bold'> &quot" . $searchkey . "&quot </span>"; ?></h1>
+      <?php if(count($result_users) || ($result_kenmerken)  > 0 ): ?>
+        <h1> <?php echo count($result_users) + count($result_kenmerken) . " Search Result(s) found for " . "<span style = 'font-weight: bold'> &quot" . $searchkey . "&quot </span>"; ?></h1>
     <?php else: ?>
       <h1>No results found </h1>
       <?php endif; ?>
@@ -42,6 +63,12 @@ if(isset($_GET['search'])){
       <br>
      <p><span style="font-weight: bold">Username:</span> <?php echo $users['user_name']; ?></p>
      <p><span style="font-weight: bold">Full name:</span> <?php echo $users['first_name'] . " " . $users['last_name']; ?></p>
+    <br>
+     <p><span style="font-weight: bold">Movie:</span> <?php echo $kenmerken['movie']; ?></p>
+     <p><span style="font-weight: bold">Destination:</span> <?php echo $kenmerken['destination']; ?></p>
+     <p><span style="font-weight: bold">Cookie:</span> <?php echo $kenmerken['cookie']; ?></p>
+     <p><span style="font-weight: bold">Serie:</span> <?php echo $kenmerken['serie']; ?></p>
+     <p><span style="font-weight: bold">Hangout:</span> <?php echo $kenmerken['hangout']; ?></p>
      </nav>
 </div>
 
