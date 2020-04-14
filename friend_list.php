f3265c230576de73bcb795c5d60c36a0d9742150
brovnie <maarl.deenis@gmail.com>
2020-04-14 19:35:24 +0200

    created UserFriendlist.class, created getFriendList function  and connected to friend_list.php

---
+++ friend_list.php
@@ -0,0 +1,16 @@
+<?php
+// FREATURE 9
+// Displays user's friend lists
+// 
+//
+include_once("classes/UserFriendlist.class.php");
+
+?>
+<div>
+<h1>Friend list</h1>
+<div class="friend-container ">
+<img src="" alt="user photo">
+<h2>User name</h2>
+
+</div>
+</div>
