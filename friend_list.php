572bf5e940d98845bbc2957e5848035277ded050
brovnie <maarl.deenis@gmail.com>
2020-04-15 01:22:40 +0200

    display friends first name, last name and profile photo

--- friend_list.php
+++ friend_list.php
@@ -5,45 +5,30 @@
 //
 include_once("classes/Db.class.php");
 include_once("classes/Friendlist.class.php");
+//for user photo
+include_once("classes/EditProfile.class.php");
 
 $user = new Friendlist();
 $user->setUserId(1);
 
 $activeUser = $user->getUserId();
 
-//get list of friends
+//------------- get list of friends id's
 $connections = $user->getFriendList();
 $friendList = createFriendList($connections, $activeUser);
 
-echo '<pre>';
-var_dump($friendList);
-echo '</pre>';
-//
-
-// remove user from connections
+// create list of user friends ids
 function createFriendList($arr, $activeUser){
-    
-    //check userid_1
+
     $column1 = cleanArray($arr, 'user1_id', $activeUser);
     $column2 = cleanArray($arr, 'user2_id', $activeUser);
-    /*for($i = 0; $i < sizeof($arr); $i++){
-        if($arr[$i]["user1_id"] != $user){
-            array_push($newArr, $arr[$i]["user1_id"]);
-        }
-    }
-    //check userid_2
-    for($i = 0; $i < sizeof($arr); $i++){
-        if($arr[$i]["user2_id"] != $user){
-            array_push($newArr, $arr[$i]["user2_id"]);
-        }
-    }*/
+
     $newArr = array_merge($column1, $column2);
     return $newArr;
 }
 
-// loop
+// remove user from connections
 function cleanArray($arr, $column, $activeUser){
-    
     $newArr = [];
 
     for($i = 0; $i < sizeof($arr); $i++){
@@ -54,13 +39,21 @@ function cleanArray($arr, $column, $activeUser){
     return $newArr;
 }
 
+//----------- get informations about friends
 
 ?>
 <div>
 <h1>Friend list</h1>
+<?php foreach($friendList as $friendId):?>
+<?php
+    // get info about friend
+    $friend = new EditProfile();
+    $friend->setUserId($friendId);
+    $friendsProfile = $friend->getUserInfo();
+    ?>
 <div class="friend-container ">
-<img src="" alt="user photo">
-<h2>User name</h2>
-
+<img src="<?php echo "data/profile/" . $friendsProfile[0]['id'] ."-". $friendsProfile[1]['image_name']; ?>" alt="user photo">
+<h2><?php echo $friendsProfile[0]['first_name'] . " " . $friendsProfile[0]['last_name']; ?></h2>
 </div>
+<?php endforeach; ?>
 </div>
