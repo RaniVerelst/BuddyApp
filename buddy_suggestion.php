b0957202303921128c52037bfdca185ad218065d
brovnie <maarl.deenis@gmail.com>
2020-05-01 02:18:15 +0200

    get characteristics from current user and set up characteristics to find buddy

--- buddy_suggestion.php
+++ buddy_suggestion.php
@@ -1,5 +1,21 @@
 <?php
+
 //find potentiele match
+$mentor = new BuddySuggestion();
+
+//characteristics of current user for beter match
+$mentor->setUserid($currentUser);
+$currentUserCharacteristics = $mentor->getAllCharacteristics();
+
+//set up characteristics to beter compare
+$mentor->setMovie($currentUserCharacteristics['movie']);
+$mentor->setDestination($currentUserCharacteristics['destination']);
+$mentor->setSerie($currentUserCharacteristics['serie']);
+$mentor->setCookie($currentUserCharacteristics['cookie']);
+$mentor->setHangout($currentUserCharacteristics['hangout']);
+$mentor->setSkills($buddySkills); 
+
+
 // start conversation
 ?>
 <div>
