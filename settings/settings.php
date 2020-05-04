b31c97bb6834a785604dcdd6e30f458bcb099c5d
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-05-05 00:04:28 +0200

    Update settings.php

--- settings/settings.php
+++ settings/settings.php
@@ -11,9 +11,9 @@ const SETTINGS = [
 
 const SETTINGS2 = [
     "db" => [
-        "user" => "buds",
-        "password" => "budsimd"/*root*/,
+        "user" => "raniveq332",
+        "password" => "5o2zdnjh"/*root*/,
         "host" => "185.182.57.54",
-        "db" => "raniveq332_buds "
+        "db" => "raniveq332_buds"
     ]
 ];
