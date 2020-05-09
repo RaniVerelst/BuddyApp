eeed39c6bebd2244b92992d2061866aa2e2e83af
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-05-09 16:15:43 +0200

    new index counters

--- new/index.php
+++ new/index.php
@@ -1,3 +1,13 @@
+<?php
+// FEATURE 13
+// Display registered users and connections
+include_once("classes/Db.class.php");
+include_once("classes/Friendlist.class.php");
+
+$users = new Friendlist();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,8 +28,8 @@
     <?php include_once("includes/header.php") ?>
 
 
-    <div class="row centered index">
-        <div class="cole-sm-6 text-center">
+    <div class="centered index">
+        <div class="text-center">
             <h1>WELKOM BIJ BUDS </h1>
             <p>Vind een leuke buddy om je wegwijs te maken in het IMD leven.</p>
             <p> Heb je het IMD leven al helemaal onder de knie?</br>Word dan als Buddy het aanspreekpunt voor de
@@ -28,13 +38,27 @@
         </div>
     </div>
 
-    <div>
-        <div>
+
+    <div class="row index justify-content-center ">
+        <div class="col-lg-3 text-center">
+        </div>
+        <div class=" countr col-lg-3 text-center">
+            <p class="count"><?php echo $users->countUsers(); ?> </p>
+            <p class="strong">actieve buddys</p>
+        </div>
+        <div class=" countr col-lg-3 text-center">
+            <p class="count"><?php echo $users->countConnections(); ?> </p>
+
+            <p>buddy connecties 4 life gemaakt</p>
 
         </div>
-        <div>
-
+        <div class="col-lg-3 text-center">
         </div>
+    </div>
+
+    <div class="maybefriends">
+        <h4>VOORGESTELD VOOR JOU</h4>
+
     </div>
 
 </body>
